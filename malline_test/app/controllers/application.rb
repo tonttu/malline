6964677d957713be6584a206c6a066ba06e1dbce
Riku Palomäki <riku@palomaki.fi>
2008-05-24 13:17:21 +0000

    Empty Rails (2.0.2) site for making better testing with Malline & Rails

---
+++ malline_test/app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '1141dc9f1cddc33f870b1823549d77d4'
+end
