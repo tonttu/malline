e43ab8d63cedcde097b923f71ea44a2c7157d1cf
Riku Palomäki <riku@palomaki.fi>
2008-07-01 22:46:51 +0000

    Move fragment_cache to rails unit tests

--- malline_test/test/unit/malline_test_test.rb
+++ malline_test/test/unit/malline_test_test.rb
@@ -1,8 +1,14 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require 'vendor/plugins/malline/test/malline_test_helpers.rb'
+require 'malline.rb'
 
 class MallineTestTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+	include Malline
+	include MallineTestHelpers
+	def test_examples
+		Dir.glob(File.join(File.dirname(__FILE__), 'code', '*.mn')).each do |file|
+			assert_xml_equal(File.read(file.sub(/\.mn$/, '.target')),
+				Base.new(View.new).render(File.read(file))+"\n", file)
+		end
+	end
 end
